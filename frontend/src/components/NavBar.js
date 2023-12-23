32e10b85cddac39071ff7053336c8faeb48ee20c
Grant Bentley <gbentley0618@comcast.net>
2023-12-23 16:08:35 -0600

    add Navbar title

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import {Button, Container, Navbar, Modal, Image} from 'react-bootstrap'
+
+
+export default function NavBar() {
+
+  return (
+    <Navbar expand='lg' className='nav'>
+        <Container>
+            <h2 className='Brand-Name'>U.S. Shopper</h2> {/*Place Holder Website Name */}
+
+        </Container>  
+    </Navbar>
+  )
+}
+
