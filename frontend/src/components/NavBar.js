26f322c51fb62bd9aa8d141d5e47e2ed1de8a389
Grant Bentley <gbentley0618@comcast.net>
2023-12-28 12:29:01 -0600

    Color websight title

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,12 +3,22 @@ import {Button, Container, Navbar, Modal, Image} from 'react-bootstrap'
 
 export default function NavBar() {
 
+  const GoToCart = () => {
+    
+  }
+
   return (
-    <div>
+    <div> 
     <Navbar expand='lg' className='nav'>
         <Container>
           <div className='closed-nav-bar'>
-            <h2 className='Brand-Name'>U.S. Shopper</h2> {/*Place Holder Website Name */}
+            <div className='Brand-Name'>{/*Place Holder Website Name */}
+            <h2 className='U'>U</h2>
+              <h2>. </h2>
+              <h2 className='S' >S</h2>
+              <h2>.</h2>
+              <h2 className='Shopper'> Shopper</h2> 
+             </div>
             <button>Check Out <Image /></button>
           </div>
 
