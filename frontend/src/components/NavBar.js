a8afc928c7fb6e790e18a2f77f1d107ff22bbb35
Grant Bentley <gbentley0618@comcast.net>
2024-01-01 19:30:44 -0600

    Nav bar title & Shopping cart Button styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -22,27 +22,32 @@ export default function NavBar() {
           <div className='closed-nav-bar'>
             <div className='Brand-Name'>{/*Place Holder Website Name */}
             <h2 className='U'>U</h2>
-              <h2>. </h2>
               <h2 className='S' >S</h2>
-              <h2>.</h2>
               <h2 className='Shopper'> Shopper</h2> 
              </div>
              <Button className= 'Check-Out-Button' type='button' onClick={handleShow}>
-                <FontAwesomeIcon className='Check-Out-Button.icon' icon={faCartShopping} /> 
-                <p>Check Out</p> 
+                <div className='icon'>
+            <FontAwesomeIcon icon={faCartShopping} /> 
+                </div>
+                <p className='text'>Check Out</p> 
               </Button>
           </div>
 
           
           <div className='display-modal'>
-            <Modal show={show} onHide={handleClose}>
-              <Modal.Header>
-                <Modal.Title>Your Shopping Cart</Modal.Title>
-                <Button type='button' className='Close-Modal' onClick={handleClose}>X</Button>
-                <Modal.Body className='modal-body'>
+            <Modal className='Modal-Window' show={show} onHide={handleClose}>
+              
+                <div className='Header'>
+                  <Modal.Header className='Header'>
+                    <Modal.Title className='Shopping-Cart-Title' >Your Shopping Cart</Modal.Title>
+                    <Button type='button' className='Close-Modal' onClick={handleClose}>X</Button>
+                  </Modal.Header>
+                </div>  
+                <div className='body'>
+                <Modal.Body >
                   <p>There are no items in your shopping cart at this time.</p>
                 </Modal.Body>
-              </Modal.Header>  
+                </div>
             </Modal>
           </div>
         </Container>  
