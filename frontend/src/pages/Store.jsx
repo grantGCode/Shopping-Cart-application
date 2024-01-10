4f7f271c3ac1549032a9ba97613e1892dbfbc059
Grant Bentley <gbentley0618@comcast.net>
2024-01-09 19:58:19 -0600

    seporate Product with margin and change <li> to <Col>

--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -1,7 +1,6 @@
 import Navbar from '../components/NavBar.js'
 import {products, getProductData} from '../productStore.js'
 import {Row, Col} from 'react-bootstrap'
-// import Col from 'react-bootstrap/Col'
 
 
 function store() {
@@ -9,10 +8,8 @@ function store() {
     <div>
       <Navbar />
       <h2>store.js</h2>
-        <Row style={{display: 'block', backgroundColor: 'lightBlue'}}>
-          <Col class="font-weight-bold">{products.map((products, index) => (
-              <li className='products' key={index}>{products.title}{` $${products.price}`}</li>))}
-          </Col>
+        <Row style={{display: 'block', padding:10, margin:95, backgroundColor: 'lightBlue'}}>{products.map((products, index) => (
+              <Col style={{margin:12}} className='products'  key={index}>{products.title}{` $${products.price}`}</Col>))}
         </Row>
     </div>
   )
