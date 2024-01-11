e6d2950b076fdae2a1f7b55b0bb5efc2c4258472
Grant Bentley <gbentley0618@comcast.net>
2024-01-10 19:56:49 -0600

    formait code in Store.jsx to look cleanner

--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -8,8 +8,22 @@ function store() {
     <div>
       <Navbar />
       <h2>store.js</h2>
-        <Row style={{display: 'block', padding:10, margin:95, backgroundColor: 'lightBlue'}}>{products.map((products, index) => (
-              <Col style={{margin:12}} className='products'  key={index}>{products.title}{` $${products.price}`}</Col>))}
+        <Row style=
+          {{
+            display: 'block',
+            padding:10, 
+            margin:95, 
+            backgroundColor: 'lightBlue'
+          }}>
+          {products.map((products, index) => (
+            <Col 
+              style={{margin:12}} 
+              className='products'  
+              key={index}
+            >
+            {products.title}{` $${products.price}`}
+          </Col>
+        ))}
         </Row>
     </div>
   )
