f32ee51afe637a67ae67ad4f8c7f7f2999ef2bbb
Grant Bentley <gbentley0618@comcast.net>
2024-01-10 18:48:24 -0600

    pull from repo

--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -1,13 +1,16 @@
 import Navbar from '../components/NavBar.js'
-import products from '../productStore.js'
-import getProductData from '../productStore.js'
+import {products, getProductData} from '../productStore.js'
+import {Row, Col} from 'react-bootstrap'
+
 
 function store() {
   return (
     <div>
-      <h1>store.js</h1>
-      {/* <h3>{products}</h3> */}
       <Navbar />
+      <h2>store.js</h2>
+        <Row style={{display: 'block', padding:10, margin:95, backgroundColor: 'lightBlue'}}>{products.map((products, index) => (
+              <Col style={{margin:12}} className='products'  key={index}>{products.title}{` $${products.price}`}</Col>))}
+        </Row>
     </div>
   )
 }
