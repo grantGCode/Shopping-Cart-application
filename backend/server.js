bb048cb3f302b073572c44ca641965bd94772e5b
Grant Bentley <gbentley0618@comcast.net>
2024-03-09 22:09:01 -0600

    server running on postman

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,12 @@ const app = express();
 const stripe = require('stripe')('pk_test_51OqfYeDvSYddFJCDEAJJwjLZAkUUYeKLQnSdhjFgYy9Kx9O9wHZC2F9INktcq1AleTOZgrM3Kx0PLtlZXvcuNv2N00Q4mevZrS')
 
 
-app.use(cors());
-app.use(express.static('public'));
+// app.use(cors());
+// app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Successful response.');
 });
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'))
+app.listen(5000, () => console.log('Example app is listening on port 5000.'))
