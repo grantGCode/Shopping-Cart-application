48261e2788e486c7980c6f41cdd109f94f653e56
Grant Bentley <gbentley0618@comcast.net>
2024-03-27 12:25:11 -0500

    add id key for stripe product API keys to products array in productStore.js

--- frontend/src/productStore.js
+++ frontend/src/productStore.js
@@ -1,9 +1,9 @@
 export const products = [
-{ price: 1, title: 'Product 1', cost: 10.99 },
-{ price: 2, title: 'Product 2', cost: 17.95 },
-{ price: 3, title: 'Product 3', cost: 8.59 },
-{ price: 4, title: 'Product 4', cost: 25.00 },
-{ price: 5, title: 'Product 5', cost: 19.99 }
+{id:`${process.env.PRODUCT_ONE_KEY}`, price: 1, title: 'Product 1', cost: 10.99 },
+{id: `${process.env.PRODUCT_TWO_KEY}`, price: 2, title: 'Product 2', cost: 17.95 },
+{id: `${process.env.PRODUCT_THREE_KEY}`, price: 3, title: 'Product 3', cost: 8.59 },
+{id: `${process.env.PRODUCT_FORE_KEY}`, price: 4, title: 'Product 4', cost: 25.00 },
+{id: `${process.env.PRODUCT_FIVE_KEY}`, price: 5, title: 'Product 5', cost: 19.99 }
 ]
 
 
