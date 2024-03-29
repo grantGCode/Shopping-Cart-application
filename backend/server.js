76e6ddeb0e1c17d575a0a820e53aacbde1fad306
Grant Bentley <gbentley0618@comcast.net>
2024-03-29 18:45:21 -0500

    set Server.js to post to Stripe API if res. status === 200 console will retun successs

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+try {
+  require('dotenv').config();
+} catch (error) {
+  console.error('Error loading .env file:', error);
+}
+const stripeKey = process.env.REACT_APP_STRIPE_API_KEY
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
-const stripe = require('stripe')(process.env.STRIPE_API_KEY)
+const stripe = require('stripe')(stripeKey)
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -11,23 +16,34 @@ app.use(cors({
   origin: 'http://localhost:3000'
 }));
 
-app.get('/', (req, res) => {
-  console.log(req.body)
-  res.send(req.body)
-})
-
-
 app.post('/', async (req, res) => {
   try{
-    const lineItem = req.body
-    console.log(lineItem)
+    const Items = req.body
+    const lineItems = Items.map((item)=>{
+      return {
+        price: item.stripeId, 
+        quantity: item.quantity
+      }
+    })   
+
+    if(lineItems === undefined || 
+      !Array.isArray(lineItems)) {
+        console.error('Line Items is not set as a array.')
+        return null;
+    }
+
     const session = await stripe.checkout.sessions.create({
-      line_item: lineItem,
+      line_items: lineItems,
       mode: 'payment',
       success_url: 'http://localhost:3000/Success.jsx',
       cancel_url:  'http://localhost:3000/Cancel.jsx',
     })
-    res.send(JSON(session.url))
+    res.send(JSON.stringify(session.url))
+
+    if (res.status(200)){
+      console.log('success')
+    }
+
   }catch (e) {
     res.status(500).json({error: e.message})
     console.log(e.message)
